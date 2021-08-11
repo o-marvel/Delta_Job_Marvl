08e9bacd876fc5152654f04cc63bdb8ac4cae56d
o-marvel <osemekemrvlls@gmail.com>
2021-08-11 18:02:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/deltajob/ui/JobDetail.kt
@@ -0,0 +1,12 @@
+package com.example.deltajob.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.deltajob.R
+
+class JobDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_detail)
+    }
+}
